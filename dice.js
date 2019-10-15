fc047ff44d74419705dd49f03715605a6b69fe20
Nathan Greenlaw <ngreenlaw@cbtnuggets.com>
2019-10-14 17:33:14 -0700

    broken table manipulations

--- dice.js
+++ dice.js
@@ -1,46 +1,58 @@
 function roll_dice(dice_num, dice_options) {
     var i;
-    var results = [];
+    var results = {};
     for(i=0; i<dice_num; i++){
       var randomNumber = Math.floor(Math.random() * dice_options.length);
-      results.push(dice_options[randomNumber]);
+      results[i] = dice_options[randomNumber];
     }
     return results;
   }
 
-function fillTable(result, tab, head, dice_options){
+function fillTable(result, tab, head, dice_options, column){
   var table = document.getElementById(tab);
   table.cellPadding = '10';
+  //Clear table
   for(var i = 0;i<table.rows.length;){
     table.deleteRow(i);
   }
+  //Create Header row
   var header = table.createTHead();
-  var row = header.insertRow(0);    
-  var cell = row.insertCell(0);
+  var row = header.insertRow(0); 
+  //Fill necessary Cells
+  for (var i=0; i<column; i++){
+    row.insertCell(i);
+    row.insertCell(-1*i);
+  }
+  var cell = row.insertCell(column);
   cell.innerHTML =  head;
-  //cell.bgColor = 'white'; 
-  for (var i = 0; i < result.length; i++)
+  
+  for (var key in result)
   {
-    let row = table.insertRow(i+1);
-    var cell1 = row.insertCell(0);
-    cell1.innerHTML = '<img src='+result[i] +' alt="" width="50%" height = "50%"/>';//result[i];
+    val = parseInt(key);
+    let row = table.insertRow(val+1);
+    for (var i=0; i<column; i++){
+      row.insertCell(i);
+      row.insertCell(-1*i);
+    }
+    var cell1 = row.insertCell(column);
+    cell1.innerHTML = '<img src='+result[val] +' alt="" width="50%" height = "50%"/>';//result[i];
     //cell1.bgColor = 'white'; 
-    var exp_roll = result[i];
-    var count = 1;
+    var exp_roll = result[val];
+    var count = val;
     while (String(exp_roll).includes('Success_Exp')){
       var exp_roll = roll_dice(1, dice_options);
       var cell2 = row.insertCell(count);
-      cell2.innerHTML = '<img src='+exp_roll +' alt="" width="50%" height = "50%"/>';//exp_roll;
+      cell2.innerHTML = '<img src='+exp_roll[0] +' alt="" width="50%" height = "50%"/>';//exp_roll;
       //cell2.bgColor = 'white'; 
       count += 1;
     }
   }
 }
 
-function button_click(dice_input_id, dice_options, table_id, header){
+function button_click(dice_input_id, dice_options, table_id, header, column){
   var dice_num = document.getElementById(dice_input_id).value;
   var result = roll_dice(dice_num, dice_options);
-  fillTable(result, table_id, header, dice_options);
+  fillTable(result, table_id, header, dice_options, column);
 }
 
 var bbutton = document.getElementById('bd_roll');
@@ -49,14 +61,14 @@ var abutton = document.getElementById('ad_roll');
 //['success', 'opportunity', 'blank', 'explosive success/strife', 'opportunity/strife', 'success/strife'];
 
 bbutton.onclick = function(){
-  button_click('bd_num', ['Opp.png', 'Strife.png', 'Success_Exp.png', 'Success.png'],'bd_table', "<b>Black Dice Results</b>");
+  button_click('bd_num', ['Opp.png', 'Strife.png', 'Success_Exp.png', 'Success.png'],'bd_table', "<b>Black Dice Results</b>", 0);
 };
 
 wbutton.onclick = function(){
-  button_click('wd_num', ['Opp.png', 'Strife.png', 'Success_Exp.png', 'Success.png'],'wd_table', "<b>White Dice Results</b>");
+  button_click('wd_num', ['Opp.png', 'Strife.png', 'Success_Exp.png', 'Success.png'],'wd_table', "<b>White Dice Results</b>", 2);
 };
 
 abutton.onclick = function() {
-  button_click('bd_num', ['Opp.png', 'Strife.png', 'Success_Exp.png', 'Success.png'],'bd_table', "<b>Black Dice Results</b>");
-  button_click('wd_num', ['Opp.png', 'Strife.png', 'Success_Exp.png', 'Success.png'],'wd_table', "<b>White Dice Results</b>");
+  button_click('bd_num', ['Opp.png', 'Strife.png', 'Success_Exp.png', 'Success.png'],'bd_table', "<b>Black Dice Results</b>", 0);
+  button_click('wd_num', ['Opp.png', 'Strife.png', 'Success_Exp.png', 'Success.png'],'bd_table', "<b>White Dice Results</b>", 2);
 };
